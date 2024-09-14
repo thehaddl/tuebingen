365cf706f86a959d511ec616376995170ddb8242
Thaddeus Mueller-Sarnowski <thaddeus@mueller-sarnowski.de>
2024-09-14 16:36:54 +0200

    remove unused method and fix build

--- src/main/java/Particle.java
+++ src/main/java/Particle.java
@@ -7,14 +7,4 @@ class Particle{
         this.velocity = vel;
         this.force = f;
     }
-
-    public double[][] particleToArray() {
-        double[][] p = new double[3][];
-        p[0] = position.getComponents();
-        p[1] = velocity.getComponents();
-        p[2] = force.getComponents();
-
-        return p;
-    }
-
 }
