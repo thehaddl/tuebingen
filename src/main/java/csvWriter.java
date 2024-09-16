a50211cb6b252aa54b9d74ef63649e44033bcfa1
Thaddeus Mueller-Sarnowski <thaddeus@mueller-sarnowski.de>
2024-09-16 13:50:06 +0200

    quick fix problem when reading in file in python, one empty column

--- src/main/java/csvWriter.java
+++ src/main/java/csvWriter.java
@@ -8,7 +8,7 @@ public class csvWriter {
     }
     void writeParticlesToCsv(Particle[] particles) throws IOException {
         try (FileWriter writer = new FileWriter(filePath)) {
-            writer.write("Position_X,Position_Y,Position_Z,Velocity_X,Velocity_Y,Velocity_Z\n");
+            writer.write("Position_X,Position_Y,Position_Z,Velocity_X,Velocity_Y,Velocity_Z,\n");
             for (Particle p : particles) {
                 writer.write(p.toCsvString() + "\n");
             }
