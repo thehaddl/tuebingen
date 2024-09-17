643c981b37e5f1b02879779869f73c6acf5fb69d
Thaddeus Mueller-Sarnowski <thaddeus@mueller-sarnowski.de>
2024-09-17 17:50:37 +0200

    quick fix to delete file before initiating new one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 class Main {
     public static void main(String[] args) throws IOException {
-        Simulation s = new Simulation(100, 50, 100, 2);
+        File f = new File("pythonVisuals/particles.csv");
+        f.delete();
+        Simulation s = new Simulation(10, 50, 30, 0.05);
         s.initRandParticles();
         s.runSim();
         System.out.println(s.particles.length);
