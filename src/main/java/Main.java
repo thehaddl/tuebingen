fb90307a848091976583213f554a3e35164c87be
Thaddeus Mueller-Sarnowski <thaddeus@mueller-sarnowski.de>
2024-09-19 16:24:39 +0200

    improve readdata method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,26 @@ import java.io.IOException;
 class Main {
     public static void main(String[] args) throws IOException {
         File f = new File("pythonVisuals/particles.csv");
+        File g = new File("pythonVisuals/particles2.csv");
         f.delete();
-        Simulation s = new Simulation(10, 50, 30, 0.05);
+        g.delete();
+        Simulation s = new Simulation(20, 2, 1000, 0.01);
         s.initRandParticles();
+        s.initialParticles = new Particle[s.particles.length];
+        for (int i = 0; i < s.particles.length; i++) {
+            s.initialParticles[i] = new Particle(s.particles[i].position, s.particles[i].velocity); // Create a new instance
+        }
+        System.out.println(s.initialParticles[1].velocity.getMagnitude());
+
         s.runSim();
-        System.out.println(s.particles.length);;
+        System.out.println(s.initialParticles[1].velocity.getMagnitude());
+        s.particles = new Particle[s.initialParticles.length];
+        for (int i = 0; i < s.particles.length; i++) {
+            s.particles[i] = new Particle(s.initialParticles[i].position, s.initialParticles[i].velocity); // Create a new instance
+        }
+        System.out.println(s.initialParticles[1].velocity.getMagnitude());
+        s.runSimSubsetOfN();
+
     }
 }
 
