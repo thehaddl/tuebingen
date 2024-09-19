bf70607507c945ba5594adddfc1088af366183cb
Thaddeus Mueller-Sarnowski <thaddeus@mueller-sarnowski.de>
2024-09-19 16:24:51 +0200

    improve readdata method

--- pythonVisuals/particle_animation.py
+++ pythonVisuals/particle_animation.py
@@ -1,29 +1,30 @@
 import pandas as pd
 from manim import *
-
+config.disable_caching = False
 def readData():
-    df_temp = pd.read_csv('particles.csv', nrows=0)
-    columns_to_read = df_temp.columns[:-1]
-    d = pd.read_csv('particles.csv', usecols=columns_to_read)
-    return d
+    df = pd.read_csv('particles.csv', header= None)
+    df = df.dropna(axis=1, how='all')
+    columns = ['x1', 'x2', 'x3', 'vx', 'vy', 'vz']  # Define column names based on your data
+    df.columns = columns
+    return df
 df = readData()
 scale_factor = 10
 print(df.head())
 
 class Animation(ThreeDScene):
     def construct(self):
-        num_particles = 5
+        num_particles = 20
         num_time_steps = len(df) // num_particles
-        particle_radius = 0.1 
+        particle_radius = 0.07 
         particles = [Sphere(radius=particle_radius, color=BLUE) for _ in range(num_particles)]
         axes = ThreeDAxes()
         self.add(axes)
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
 
         for i, particle in enumerate(particles):
-            initial_pos = [df.iloc[i][0],
-                           df.iloc[i][1],
-                           df.iloc[i][2]]
+            initial_pos = [df.iloc[i]['x1'],
+                           df.iloc[i]['x2'],
+                           df.iloc[i]['x3']]
             particle.move_to(initial_pos)
             self.add(particle)
 
@@ -34,14 +35,14 @@ class Animation(ThreeDScene):
             for i, particle in enumerate(particles):
                 row_index = i + t * num_particles
                 if row_index < len(df):
-                    new_pos = [df.iloc[row_index][0],
-                               df.iloc[row_index][1],
-                               df.iloc[row_index][2]]
+                    new_pos = [df.iloc[row_index]['x1'],
+                               df.iloc[row_index]['x2'],
+                               df.iloc[row_index]['x3']]
 
                     print(f"Particle {i} - Time Step {t}: New Position: {new_pos}")
                     animations.append(particle.animate.move_to(new_pos))
 
-            self.play(*animations, run_time=0.4, rate_func = smooth)
+            self.play(*animations, run_time=0.1, rate_func = smooth)
             self.wait(0.1)
 
         self.wait(2)
