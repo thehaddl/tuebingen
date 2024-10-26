dcbda5eec74f688278de8a790a0e210044b924fd
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2024-10-26 21:07:22 +0200

    fix Deviator class in method calcDeviation()

--- src/main/java/Deviator.java
+++ src/main/java/Deviator.java
@@ -1,24 +1,27 @@
-public class Deviator
-{
+import java.sql.SQLOutput;
+
+public class Deviator {
     double[] deviationPos;
     double[][] deviationPosPerTime;
-    Deviator(int numParticles){
+
+    Deviator(int numParticles) {
         this.deviationPos = new double[numParticles];
     }
-    public double[] calcDeviation(Particle[] p1, Particle[]p2){
-        for(int i = 0;i < p1.length;i++){
+
+    public double[] calcDeviation(Particle[] p1, Particle[] p2) {
+        for (int i = 0; i < p1.length; i++) {
             Particle a = p1[i];
             Particle b = p2[i];
-            deviationPos[i] = Math.abs(a.position.subtract(b.position).getMagnitude());
+            deviationPos[i] = a.position.subtract(b.position).getMagnitude();
         }
         return deviationPos;
     }
-    public double[][] calcDeviationPerTime(Particle[] p1, Particle[]p2, int k){
-        for(int i = 0;i < p1.length;i++){
-            Particle a = p1[i];
-            Particle b = p2[i];
-            deviationPosPerTime[i][k] = Math.abs(a.position.subtract(b.position).getMagnitude());
+
+    public double averageDev(double[] deviation) {
+        double dev = 0;
+        for (int i = 0; i < deviation.length; i++) {
+            dev += deviation[i];
         }
-        return deviationPosPerTime;
+        return dev / (deviation.length);
     }
 }
