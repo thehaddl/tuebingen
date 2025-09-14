71ff11a123c56624d052f978f09e1c52ae8cbda7
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-09-14 22:00:15 +0200

    change unittest accordingly

--- src/test/java/ParticleSystemTest.java
+++ src/test/java/ParticleSystemTest.java
@@ -54,23 +54,5 @@ public class ParticleSystemTest {
         }
     }
 
-    @Test
-    void calcAverageDeviation_should_return_average_deviation() {
-        // setup
-
-        List<Particle> p1 = new ArrayList();
-        p1.add(new Particle(new Vector(1,2,3), new Vector(2,3,4)));
-        p1.add(new Particle(new Vector(3,4,5), new Vector(4,5,6.1)));
-        ParticleSystem current = ParticleSystem.createFrom(p1);
-        List<Particle> p2 = new ArrayList();
-        p2.add(new Particle(new Vector(1.1,2,3), new Vector(2.1,3,4)));
-        p2.add(new Particle(new Vector(3.1,4,5.1), new Vector(4,5.1,6)));
-
-        ParticleSystem reference = ParticleSystem.createFrom(p2);
-        //execute
-        var actual = current.calcAverageDeviation(reference);
-        // verify
-        assertEquals((Math.sqrt(1)+ Math.sqrt(2))/20 ,actual,0.00001);
-    }
 
 }
