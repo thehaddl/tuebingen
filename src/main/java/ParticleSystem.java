eb48bbf02c742fcbef21b94f10a5cf68855af7d7
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-09-14 21:58:28 +0200

    implement method to measure mdtrics stored in SimulationComparer

--- src/main/java/ParticleSystem.java
+++ src/main/java/ParticleSystem.java
@@ -70,16 +70,142 @@ public class ParticleSystem {
         return new ParticleSystem(copy);
 
     }
-
-    public double calcAverageDeviation(ParticleSystem reference) {
-        double deviation = 0.0;
-
-        for (int i = 0; i < this.particles.size(); i++) {
-            Particle a = this.particles.get(i);
-            Particle b = reference.particles.get(i);
-            deviation += a.position.subtract(b.position).getMagnitude();
+    public SimulationComparer compare(ParticleSystem reference){
+        if (this.particles.size() != reference.particles.size()) {
+            throw new IllegalArgumentException("Cannot compare systems with different particle counts");
         }
-        return deviation / this.particles.size();
-    }
+        // Position errors
+        double[] posErrors = calculatePositionError(reference);
 
+        // Velocity errors
+        double[] velErrors = calculateVelocityError(reference);
+
+        // Energy errors
+        double[] energyErrors = calculateEnergyErrors(reference);
+
+        // Structural errors
+        double comDrift = calculateCenterOfMassDrift(reference);
+        double pairDistError = calculateMeanPairDistanceError(reference);
+        double spreadError = calculateSystemSpreadError(reference);
+
+        return new SimulationComparer(
+                posErrors[0], posErrors[1], posErrors[2],  // mean, rmse, max position
+                velErrors[0], velErrors[1], velErrors[2],  // mean, rmse, max velocity
+                energyErrors[0], energyErrors[1], energyErrors[2], // kinetic, potential, total
+                comDrift, pairDistError, spreadError
+        );
+    }
+    private double[] calculatePositionError(ParticleSystem reference){
+        double sumError= 0.0;
+        double sumSquaredError = 0.0;
+        double maxError = 0.0;
+        for(int i = 0; i< particles.size();i++){
+            double error = particles.get(i).position.subtract(reference.particles.get(i).position).getMagnitude();
+            sumError += error;
+            sumSquaredError += error*error;
+            maxError = Math.max(maxError,error);
+        }
+        double mean = sumError / particles.size();
+        double rmse = Math.sqrt(sumSquaredError / particles.size());
+        return new double[]{mean, rmse, maxError};
+    }
+    private double[] calculateVelocityError(ParticleSystem reference){
+        double sumError= 0.0;
+        double sumSquaredError = 0.0;
+        double maxError = 0.0;
+        for(int i = 0; i< particles.size();i++){
+            double error = particles.get(i).velocity.subtract(reference.particles.get(i).velocity).getMagnitude();
+            sumError += error;
+            sumSquaredError += error*error;
+            maxError = Math.max(maxError,error);
+        }
+        double mean = sumError / particles.size();
+        double rmse = Math.sqrt(sumSquaredError / particles.size());
+        return new double[]{mean, rmse, maxError};
+    }
+    private double[] calculateEnergyErrors(ParticleSystem reference) {
+        double thisKinetic = calculateKineticEnergy();
+        double refKinetic = reference.calculateKineticEnergy();
+        double kineticError = Math.abs(thisKinetic - refKinetic) / Math.abs(refKinetic);
+
+        double thisPotential = calculatePotentialEnergy();
+        double refPotential = reference.calculatePotentialEnergy();
+        double potentialError = Math.abs(thisPotential - refPotential) / Math.abs(refPotential);
+
+        double thisTotal = thisKinetic + thisPotential;
+        double refTotal = refKinetic + refPotential;
+        double totalError = Math.abs(thisTotal - refTotal) / Math.abs(refTotal);
+
+        return new double[]{kineticError, potentialError, totalError};
+    }
+    private double calculateSystemSpreadError(ParticleSystem reference) {
+        double thisSpread = calculateSystemSpread();
+        double refSpread = reference.calculateSystemSpread();
+        return Math.abs(thisSpread - refSpread) / refSpread;
+    }
+    private double calculateCenterOfMassDrift(ParticleSystem reference) {
+        Vector thisCOM = this.centerOfGravity();
+        Vector refCOM = reference.centerOfGravity();
+        return thisCOM.subtract(refCOM).getMagnitude();
+    }
+    private double calculateMeanPairDistanceError(ParticleSystem reference) {
+        double sumError = 0.0;
+        int pairCount = 0;
+
+        for (int i = 0; i < particles.size(); i++) {
+            for (int j = i + 1; j < particles.size(); j++) {
+                double thisDist = particles.get(i).position
+                        .subtract(particles.get(j).position).getMagnitude();
+                double refDist = reference.particles.get(i).position
+                        .subtract(reference.particles.get(j).position).getMagnitude();
+                sumError += Math.abs(thisDist - refDist);
+                pairCount++;
+            }
+        }
+
+        return sumError / pairCount;
+    }
+    private double calculateSystemSpread() {
+        Vector com = centerOfMass();
+        double sumSquaredDistance = 0.0;
+        for (Particle p : particles) {
+            double dist = p.position.subtract(com).getMagnitude();
+            sumSquaredDistance += dist * dist;
+        }
+        return Math.sqrt(sumSquaredDistance / particles.size());
+    }
+    private double calculatePotentialEnergy() {
+        double totalPE = 0.0;
+        for (int i = 0; i < particles.size(); i++) {
+            for (int j = i + 1; j < particles.size(); j++) {
+                Particle pi = particles.get(i);
+                Particle pj = particles.get(j);
+                double r = pi.position.subtract(pj.position).getMagnitude();
+                if (r > 0) { // Avoid division by zero
+                    totalPE += pi.charge * pj.charge / r; // Coulomb potential
+                }
+            }
+        }
+        return totalPE;
+    }
+    private double calculateKineticEnergy() {
+        double totalKE = 0.0;
+        for (Particle p : particles) {
+            double v2 = p.velocity.getMagnitude();
+            v2 = v2 * v2;
+            totalKE += 0.5 * p.mass * v2;
+        }
+        return totalKE;
+    }
+    public Vector centerOfMass() {
+        var massWeightedSum = new Vector(0, 0, 0);
+        double totalMass = 0.0;
+
+        for (Particle p : this.particles) {
+            massWeightedSum = massWeightedSum.add(p.position.scale(p.mass));
+            totalMass += p.mass;
+        }
+
+        return massWeightedSum.scale(1.0 / totalMass);
+    }
 }
