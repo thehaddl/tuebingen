515c35b34320b1bffc814f67df59f94efb42aebe
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-09-15 06:50:05 +0200

    add BeforeEach setup to avoid redundant labor

--- src/test/java/ParticleSystemTest.java
+++ src/test/java/ParticleSystemTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,6 +7,33 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParticleSystemTest {
+
+    private ParticleSystem s1;
+    private ParticleSystem s2;
+    private ParticleSystem identicalSystem;
+
+    @BeforeEach
+    void setUp() {
+        List<Particle> p1 = new ArrayList<>();
+        p1.add(new Particle(new Vector(0, 0, 0), new Vector(1, 0, 0), 1.0, 1));
+        p1.add(new Particle(new Vector(1, 0, 0), new Vector(-1, 0, 0), 1.0, -1));
+        p1.add(new Particle(new Vector(0, 1, 0), new Vector(0, 1, 0), 2.0, 1));
+
+        List<Particle> p2 = new ArrayList<>();
+        p2.add(new Particle(new Vector(0.1, 0.05, 0), new Vector(1.02, 0, 0), 1.0, 1));
+        p2.add(new Particle(new Vector(2, 1, 0), new Vector(-2, 0, 0), 1.0, -1));
+        p2.add(new Particle(new Vector(1, 2, 0), new Vector(0, 2, 0), 2.0, 1));
+
+        List<Particle> identicalParticles = new ArrayList<>();
+        identicalParticles.add(new Particle(new Vector(0, 0, 0), new Vector(1, 0, 0), 1.0, 1));
+        identicalParticles.add(new Particle(new Vector(1, 0, 0), new Vector(-1, 0, 0), 1.0, -1));
+        identicalParticles.add(new Particle(new Vector(0, 1, 0), new Vector(0, 1, 0), 2.0, 1));
+
+
+        s1 = new ParticleSystem(p1);
+        s2 = new ParticleSystem(p2);
+        identicalSystem = new ParticleSystem(identicalParticles);
+    }
     @Test
     void centerOfGravity_should_return_center_of_gravity () {
         // setup
