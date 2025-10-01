cfdc415cb740139af885486692e38a8c2948dba2
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-10-01 21:18:08 +0200

    main method for comparing Simulations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,29 @@ import java.nio.file.Path;
 
 class Main {
 
-    static final int N = 5000;
-    static final int k = 2500;
+    static final int N = 500;
+    static final int k = 500;
     static final int steps = 1000;
     static int runTime = 1000;
     static final double dimension = N/100 ;
 
     public static void main(String[] args) throws IOException {
-    Simulation s= new Simulation(steps,runTime);
-    ParticleSystem initial = ParticleSystem.createRandomPositionsByDensity(N,0.1);
-    s.setForceCalculationMethod(ForceCalculationMethod.PARALLEL_MUTABLE);
-    s.setParticleSampler(new RandomParticleSampler(k));
-    ParticleSystem sample = s.runSim(initial);
-    s.setParticleSampler(new AllParticleSampler());
-    ParticleSystem total = s.runSim(initial);
+    var outputAll = new CsvWriter(Path.of("simulation.csv"));
+    var outputSample = new CsvWriter(Path.of("sampleSimulation.csv"));
+    Simulation sampleSim= new Simulation(steps,runTime);
+    ParticleSystem initial = ParticleSystem.createRandomPositionsByDensity(N,0.01);
+    initial.putIDs();
+    sampleSim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
+    sampleSim.setParticleSampler(new RandomParticleSampler(k));
+    sampleSim.setOutput(outputSample);
+    ParticleSystem sample = sampleSim.runSim(initial);
+    Simulation sim= new Simulation(steps,runTime);
+    sim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
+    sim.setParticleSampler(new AllParticleSampler());
+    sim.setOutput(outputAll);
+    ParticleSystem total = sim.runSim(initial);
     SimulationComparer sc = sample.compare(total);
-    sc.toString();
+    System.out.println(sc.toString());
     }
 
 
