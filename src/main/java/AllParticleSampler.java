4f476bf05deff579563ae27914dc541f05bdedc5
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-10-03 10:43:40 +0200

    change AllParticleSampler to return copy

--- src/main/java/AllParticleSampler.java
+++ src/main/java/AllParticleSampler.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class AllParticleSampler implements ParticleSampler {
     public List<Particle> select(List<Particle> particles, SimulationProfiler profiler) {
-        return particles;
+        return new ArrayList(particles);
     }
 }
