70b44b0f40447c714c3ff39506a12beeeca309e0
thaddeusmueller-sarnowski <thaddeus@mueller-sarnowski.de>
2025-10-03 10:23:21 +0200

    wrap output initialization in try block to trigger autocloseable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,23 +11,22 @@ class Main {
     static final double dimension = N/100 ;
 
     public static void main(String[] args) throws IOException {
-    var outputAll = new CsvWriter(Path.of("simulation.csv"));
-    var outputSample = new CsvWriter(Path.of("sampleSimulation.csv"));
-    Simulation sampleSim= new Simulation(steps,runTime);
-    ParticleSystem initial = ParticleSystem.createRandomPositionsByDensity(N,0.01);
-    initial.putIDs();
-    sampleSim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
-    sampleSim.setParticleSampler(new RandomParticleSampler(k));
-    sampleSim.setOutput(outputSample);
-    ParticleSystem sample = sampleSim.runSim(initial);
-    Simulation sim= new Simulation(steps,runTime);
-    sim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
-    sim.setParticleSampler(new AllParticleSampler());
-    sim.setOutput(outputAll);
-    ParticleSystem total = sim.runSim(initial);
-    SimulationComparer sc = sample.compare(total);
-    System.out.println(sc.toString());
+        try (var outputAll = new CsvWriter(Path.of("simulation.csv"));
+             var outputSample = new CsvWriter(Path.of("sampleSimulation.csv"))) {
+            Simulation sampleSim = new Simulation(steps, runTime);
+            ParticleSystem initial = ParticleSystem.createRandomPositionsByDensity(N, 0.001);
+            initial.putIDs();
+            sampleSim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
+            sampleSim.setParticleSampler(new RandomParticleSampler(k));
+            sampleSim.setOutput(outputSample);
+            ParticleSystem sample = sampleSim.runSim(initial);
+            Simulation sim = new Simulation(steps, runTime);
+            sim.setForceCalculationMethod(ForceCalculationMethod.NAIVE_IMMUTABLE);
+            sim.setParticleSampler(new AllParticleSampler());
+            sim.setOutput(outputAll);
+            ParticleSystem total = sim.runSim(initial);
+            SimulationComparer sc = sample.compare(total);
+            System.out.println(sc.toString());
+        }
     }
-
-
 }
